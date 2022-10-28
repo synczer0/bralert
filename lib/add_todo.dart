09e82f169db312642be781b906d2b5c7e1631730
Justin <36100434+synczer0@users.noreply.github.com>
2022-10-28 12:24:29 +0800

    Fix compatability issues and perfrom migration to support latest version of the SDK

--- lib/add_todo.dart
+++ lib/add_todo.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 // Global variable
-//
 int sizeOfPrefs;
 
 class AddTodo extends StatelessWidget {
